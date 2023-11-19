9d68d47bc1104fa408fe52c29d7c9714495e4be0
Addai Christopher <addaichristopher1@gmail.com>
2023-11-19 15:06:10 +0000

    0x19. C - Stacks, Queues - LIFO, FIFO C Group project Algorithm Data structure
    Co-Authored-By: OliviaTetteh <138711222+OliviaTetteh@users.noreply.github.com>

---
+++ errors1.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * err - print error for the error code.
+ * @error_code: The error codes:
+ */
+void err(int error_code, ...)
+{
+	va_list ag;
+	char *op;
+	int l_num;
+
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 1:
+			printf("USAGE: monty file\n");
+			break;
+		case 2:
+			printf("Error: Can't open file %s\n",
+				va_arg(ag, char *));
+			break;
+		case 3:
+			l_num = va_arg(ag, int);
+			op = va_arg(ag, char *);
+			printf("L%d: unknown instruction %s\n", l_num, op);
+			break;
+		case 4:
+			printf("Error: malloc failed\n");
+			break;
+		case 5:
+			printf("L%d: usage: push integer\n", va_arg(ag, int));
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
---
+++ read_file.c
@@ -0,0 +1,55 @@
+#include "monty.h"
+
+/**
+ * open_file - Open and read a file.
+ * @file_name: String with the name of the file.
+ */
+
+void open_file(char *file_name)
+{
+	int file_check;
+	FILE *fd;
+
+	if (file_name == NULL)
+		err(2, file_name);
+	/*Checks if the file exists*/
+	file_check = access(file_name, R_OK);
+	if (file_check == -1)
+		err(2, file_name);
+	fd = fopen(file_name, "r");
+	if (fd == NULL)
+		err(2, file_name);
+
+	/*errors should be handled inside this function*/
+	read_file(fd);
+	/*might need to check for errors on closing a file.*/
+	fclose(fd);
+}
+
+/**
+ * read_file - Reads the content of a file line by line.
+ * @fd: Pointer to a file descriptor of an open file
+ */
+
+void read_file(FILE *fd)
+{
+	int line_n;
+	int format;
+	char *lineprt;
+	size_t n;
+
+	lineprt = NULL;
+	n = 0;
+	format = 0;
+
+	if (fd == NULL)
+		err(2, "file_name");
+
+	/*Getting each line in the file*/
+	for (line_n = 1; getline(&lineprt, &n, fd) != EOF; line_n++)
+	{
+		format = interpret_line(lineprt, line_n, format);
+	}
+	free(lineprt);
+}
+
