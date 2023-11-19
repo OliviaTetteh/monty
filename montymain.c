76653cdc717064cd139cf15e83295dbac60c7b34
Addai Christopher <addaichristopher1@gmail.com>
2023-11-19 16:56:22 +0000

    fix bugs
    Co-Authored-By: OliviaTetteh <138711222+OliviaTetteh@users.noreply.github.com>

--- montymain.c
+++ montymain.c
@@ -1,11 +1,13 @@
 #include "monty.h"
-
 /**
  * main - the main function
  * @argc: Number of command line arguments.
  * @argv: An array containing the arguments.
  * Return: Always Zero.
  */
+
+stack_t *head = NULL;
+
 int main(int argc, char **argv)
 {
 	if (argc < 2 || argc > 2)
@@ -21,7 +23,6 @@ int main(int argc, char **argv)
 void free_nodes(void)
 {
 	stack_t *tmp;
-	stack_t *head = NULL;
 
 	if (head == NULL)
 		return;
